18332953d4f230b84379b7ab7a6212621772775f
Ashar Latif <ashar@kpmpower.com>
2023-03-24 11:32:50 -0400

    Added muxes and DACs

---
+++ displays/tft-ili9341/firmware/ili9341.h
@@ -0,0 +1,90 @@
+/* vim: set ai et ts=4 sw=4: */
+#ifndef __ILI9341_H__
+#define __ILI9341_H__
+
+#include "fonts.h"
+#include "main.h"
+#include <stdbool.h>
+
+#define ILI9341_MADCTL_MY  0x80
+#define ILI9341_MADCTL_MX  0x40
+#define ILI9341_MADCTL_MV  0x20
+#define ILI9341_MADCTL_ML  0x10
+#define ILI9341_MADCTL_RGB 0x00
+#define ILI9341_MADCTL_BGR 0x08
+#define ILI9341_MADCTL_MH  0x04
+
+/*** Redefine if necessary ***/
+#define ILI9341_SPI_PORT hspi1
+extern SPI_HandleTypeDef ILI9341_SPI_PORT;
+
+#define ILI9341_RES_Pin       LCD_RST_Pin
+#define ILI9341_RES_GPIO_Port LCD_RST_GPIO_Port
+#define ILI9341_CS_Pin        LCD_CS_Pin
+#define ILI9341_CS_GPIO_Port  LCD_CS_GPIO_Port
+#define ILI9341_DC_Pin        LCD_DC_Pin
+#define ILI9341_DC_GPIO_Port  LCD_DC_GPIO_Port
+
+// default orientation
+/*
+#define ILI9341_WIDTH  240
+#define ILI9341_HEIGHT 320
+#define ILI9341_ROTATION (ILI9341_MADCTL_MX | ILI9341_MADCTL_BGR)
+*/
+
+// rotate right
+/*
+#define ILI9341_WIDTH  320
+#define ILI9341_HEIGHT 240
+#define ILI9341_ROTATION (ILI9341_MADCTL_MX | ILI9341_MADCTL_MY | ILI9341_MADCTL_MV | ILI9341_MADCTL_BGR)
+*/
+
+
+// rotate left
+#define ILI9341_WIDTH  320
+#define ILI9341_HEIGHT 240
+#define ILI9341_ROTATION (ILI9341_MADCTL_MV | ILI9341_MADCTL_BGR)
+
+// upside down
+/*
+#define ILI9341_WIDTH  240
+#define ILI9341_HEIGHT 320
+#define ILI9341_ROTATION (ILI9341_MADCTL_MY | ILI9341_MADCTL_BGR)
+*/
+
+/****************************/
+
+// Color definitions
+#define BLACK      				0x0000
+#define NAVY        			0x000F
+#define DARKGREEN   			0x03E0
+#define DARKCYAN    			0x03EF
+#define MAROON      			0x7800
+#define PURPLE      			0x780F
+#define OLIVE       			0x7BE0
+#define LIGHTGREY   			0xC618
+#define DARKGREY    			0x7BEF
+#define BLUE        			0x001F
+#define GREEN       			0x07E0
+#define CYAN        			0x07FF
+#define RED         			0xF800
+#define MAGENTA     			0xF81F
+#define YELLOW      			0xFFE0
+#define WHITE       			0xFFFF
+#define ORANGE      			0xFD20
+#define GREENYELLOW 			0xAFE5
+#define PINK        			0xF81F
+#define ILI9341_COLOR565(r, g, b) (((r & 0xF8) << 8) | ((g & 0xFC) << 3) | ((b & 0xF8) >> 3))
+
+// call before initializing any SPI devices
+void ILI9341_Unselect();
+
+void ILI9341_Init(void);
+void ILI9341_DrawPixel(uint16_t x, uint16_t y, uint16_t color);
+void ILI9341_WriteString(uint16_t x, uint16_t y, const char* str, FontDef font, uint16_t color, uint16_t bgcolor);
+void ILI9341_FillRectangle(uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint16_t color);
+void ILI9341_FillScreen(uint16_t color);
+void ILI9341_DrawImage(uint16_t x, uint16_t y, uint16_t w, uint16_t h, const uint16_t* data);
+void ILI9341_InvertColors(bool invert);
+
+#endif // __ILI9341_H__
